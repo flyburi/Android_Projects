562854ee9a9f381a70edc297eed567f6d616791f
flyburi <yumi.bae@gmail.com>
2013-12-08 14:52:18 +0900

    RENAME package and classes name

--- FastLeaner/src/com/uyuni/fastlearner/db/BaseTable.java
+++ FastLeaner/src/com/uyuni/fastlearner/db/BaseTable.java
@@ -1,3 +1,4 @@
+
 package com.uyuni.fastlearner.db;
 
 import android.database.sqlite.SQLiteDatabase;
@@ -5,29 +6,30 @@ import android.util.Log;
 
 public class BaseTable {
 
-	// Database table
-	public static final String TABLE_BASE = "base_table";
-	public static final String COLUMN_ID = "_id";
-	public static final String COLUMN_CATEGORY = "category";
-	public static final String COLUMN_WORD = "word";
-	public static final String COLUMN_DEFINITION = "definition";
+    // Database table
+    public static final String TABLE_BASE = "base_table";
 
-	// Database creation SQL statement
-	private static final String DATABASE_CREATE = "create table " + TABLE_BASE
-			+ "(" + COLUMN_ID + " integer primary key autoincrement, "
-			+ COLUMN_CATEGORY + " text not null, " + COLUMN_WORD
-			+ " text not null," + COLUMN_DEFINITION + " text not null" + ");";
+    public static final String COLUMN_ID = "_id";
 
-	public static void onCreate(SQLiteDatabase database) {
-		database.execSQL(DATABASE_CREATE);
-	}
+    public static final String COLUMN_CATEGORY = "category";
 
-	public static void onUpgrade(SQLiteDatabase database, int oldVersion,
-			int newVersion) {
-		Log.w(BaseTable.class.getName(), "Upgrading database from version "
-				+ oldVersion + " to " + newVersion
-				+ ", which will destroy all old data");
-		database.execSQL("DROP TABLE IF EXISTS " + TABLE_BASE);
-		onCreate(database);
-	}
+    public static final String COLUMN_WORD = "word";
+
+    public static final String COLUMN_DEFINITION = "definition";
+
+    // Database creation SQL statement
+    private static final String DATABASE_CREATE = "create table " + TABLE_BASE + "(" + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_CATEGORY + " text not null, "
+            + COLUMN_WORD + " text not null," + COLUMN_DEFINITION + " text not null" + ");";
+
+    public static void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    public static void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+        Log.w(BaseTable.class.getName(), "Upgrading database from version " + oldVersion + " to "
+                + newVersion + ", which will destroy all old data");
+        database.execSQL("DROP TABLE IF EXISTS " + TABLE_BASE);
+        onCreate(database);
+    }
 }
--- FastLeaner/src/com/uyuni/fastlearner/db/BaseTableHelper.java
+++ FastLeaner/src/com/uyuni/fastlearner/db/BaseTableHelper.java
@@ -1,3 +1,4 @@
+
 package com.uyuni.fastlearner.db;
 
 import android.content.Context;
@@ -6,21 +7,22 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class BaseTableHelper extends SQLiteOpenHelper {
 
-	private static final String DATABASE_NAME = "basetable.db";
-	private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "basetable.db";
 
-	public BaseTableHelper(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-	}
+    private static final int DATABASE_VERSION = 1;
 
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		BaseTable.onCreate(db);
-	}
+    public BaseTableHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		BaseTable.onUpgrade(db, oldVersion, newVersion);
-	}
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        BaseTable.onCreate(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        BaseTable.onUpgrade(db, oldVersion, newVersion);
+    }
 
 }
